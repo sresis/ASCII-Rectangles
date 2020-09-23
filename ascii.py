88cd181b70be72ad75b1a17da830f2adb43830cd
Stephanie Resis <sresis@umich.edu>
2020-09-23 17:26:48 -0500

    separated make canvas func

--- ascii.py
+++ ascii.py
@@ -1,12 +1,8 @@
 
 import numpy as np
-def create_rectangle(start_x, start_y, end_x, end_y, fill_char):
-    """ Creates a rectangle."""
-    
-    # dimensions of canvas is 10x10
-    canvas_len = 10
-    canvas_height = 10
-    
+
+def create_blank_canvas(canvas_len=10, canvas_height=10):
+    """ Create a blank canvas."""
     # makes the row of the canvas length
     row = []
     i = 0
@@ -23,6 +19,13 @@ def create_rectangle(start_x, start_y, end_x, end_y, fill_char):
     
     # make rows on top of each other
     adj_matrix = np.array(matrix)
+    return adj_matrix
+
+def create_rectangle(start_x, start_y, end_x, end_y, fill_char):
+    """ Creates a rectangle."""
+    canvas = create_blank_canvas()
+    canvas_len = len(canvas)
+    canvas_height = len(canvas[0])
 
     j = 0
     while j < canvas_len:
@@ -30,11 +33,18 @@ def create_rectangle(start_x, start_y, end_x, end_y, fill_char):
             i = 0
             while i < canvas_height:
                 if i >= start_x and i <= end_x:
-                    adj_matrix[j][i] = fill_char
+                    canvas[j][i] = fill_char
                 i += 1
         j += 1
-    print(adj_matrix)
+    return canvas
+
+def change_char(char): 
+    """Changes the rectangle's fill characters."""
+    # get the rectangle
+
+    # for each non-blank character, replace it with new char
+    
 
 
-create_rectangle(1, 1, 3, 1, 'x')
-
+print(create_rectangle(1, 1, 3, 8, 'x'))
+print(create_rectangle(1, 1, 3, 4, 'o'))
